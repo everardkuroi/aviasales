597609d90281115879da2520bb1f862b425ecfbd
Alexander Romanenko <gudred.perm@gmail.com>
2018-06-19 18:42:35 +0300

    minor changes

--- src/Components/Page.jsx
+++ src/Components/Page.jsx
@@ -6,7 +6,19 @@ import Socials from "./Socials/Socials";
 import Form from "./Form/Form";
 
 export default class Page extends React.Component {
+
+  toDatabase() {
+    const request = new Request('http://localhost:3000/', {
+      method: 'POST',
+      headers: new Headers({'Content-Type': 'application/json'}),
+      body: JSON.stringify({data: 'hello'})
+    });
+
+    fetch(request).then(response => response.json().then(data => console.log(data)));
+  }
+
   render() {
+    this.toDatabase();
     return (
       <div className={'main'} style={{backgroundImage: `url(${background})`}}>
         <div className={'logo'}>
